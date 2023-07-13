550369af328865790377c79925025c946d6f41ee
Tony W <bobacrypto2@gmail.com>
2023-07-13 00:54:12 -0700

    Add images / assets to project
    Style header searchbar
    
    Style account area

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,53 @@
 import "../styles/header.css";
+import searchIcon from "../assets/images/Home/Nav/search-icon.svg";
+import logo from "../assets/images/Home/Nav/reddit-logo.svg";
+import title from "../assets/images/Home/Nav/reddit-title.svg";
+import qr from "../assets/images/Home/Nav/qrcode.svg";
+import defaultUser from "../assets/images/Home/Nav/default_user.svg";
+import arrow from "../assets/images/Default_Images/arrow-down.svg";
 
 const Header = () => {
   return (
     <nav id="header">
-      <div id="logo">
-        <img></img>
+      <div id="logo-area">
+        <a className="logo-container" href="/">
+          <img src={logo} className="reddit-logo" alt="Reddit Logo"></img>
+          <img src={title} className="reddit-title" alt="Reddit Title"></img>
+        </a>
       </div>
-      <div id="searchbar">
-        <input type="text"></input>
+      <div id="searchbar-container">
+        <form id="search-form">
+          <label htmlFor="header-search-bar">
+            <img src={searchIcon} className="search-icon"></img>
+          </label>
+          <input
+            type="text"
+            placeholder="Search Reddit..."
+            id="header-search-bar"
+          ></input>
+        </form>
       </div>
-      <div id="account-area"></div>
+      <div id="account-area">
+        <div className="header-buttons">
+          <button className="get-app">
+            <img src={qr} alt="QR Code Scan" className="qrcode" />
+            Get App
+          </button>
+          <button className="login">Log In</button>
+        </div>
+        <div className="user-profile">
+          <img
+            src={defaultUser}
+            alt="Icon of a person or user"
+            className="default-user"
+          />
+          <img
+            src={arrow}
+            alt="Small arrow pointing downwards"
+            className="down-arrow"
+          />
+        </div>
+      </div>
     </nav>
   );
 };
