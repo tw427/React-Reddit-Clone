15e5e05d13c34269adf591da59129da98332441e
Tony W <bobacrypto2@gmail.com>
2023-07-15 01:11:18 -0700

    Style sort bar above content
    Style community section for creating post or community

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -3,9 +3,14 @@ import "../styles/maincontent.css";
 const MainContent = () => {
   return (
     <main id="content-container">
-      <header>
+      <header id="sort-header">
         {/* if logged in */}
-        <div className="sortContent"></div>
+        <a className="hot-sort">Hot</a>
+        <div className="location-sort">Everywhere</div>
+        <a className="new-sort">New</a>
+        <a className="top-sort">Top</a>
+        <button className="misc-sort">...</button>
+        <div className="view-sort">View</div>
       </header>
       <section className="content"></section>
     </main>
--- src/components/SideSection.jsx
+++ src/components/SideSection.jsx
@@ -3,9 +3,35 @@ import "../styles/sidesection.css";
 const SideSection = () => {
   return (
     <section id="side-section">
-      <div className="top-communities"></div>
-      <div className="reddit-premium"></div>
-      <div className="create-post-community"></div>
+      {/* <div className="top-communities"></div> */}
+      <div className="reddit-premium">
+        <div className="premium-details">
+          <div className="prem-icon"></div>
+          <div className="prem-text">
+            <span>Reddit Premium</span>
+            <span>The best Reddit experience</span>
+          </div>
+        </div>
+        <button className="prem-btn">Try Now</button>
+      </div>
+      <div className="create-post-community">
+        <div className="upper-info">
+          <div className="info-banner"></div>
+          <div className="icon-container">
+            <div className="info-icon"></div>
+            <h2>Home</h2>
+          </div>
+          <span className="info-text">
+            Your personal Reddit frontpage. Come here to check in with your
+            favorite communities.
+          </span>
+        </div>
+        <hr></hr>
+        <div className="community-btns">
+          <button className="create-post">Create Post</button>
+          <button className="create-community">Create Community</button>
+        </div>
+      </div>
       <div className="helpful-information"></div>
     </section>
   );
